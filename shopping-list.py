6046ce9b34be237b27b40d9478f3332e97da6a86
ramireznicc23 <ramireznicc.23@pm.me>
2021-07-09 21:08:55 +0200

    Add files via upload

--- shopping-list.py
+++ shopping-list.py
@@ -1,51 +1,53 @@
 import os
 
-print('Welcome to Shopping List v1.0 by Nico.')
+title = '\nWelcome to Shopping List v1.0 by Nico.'
+print(title)
+print('*' * (len(title) - 1))
 
-lista_super = []
+shopping_list = []
 
-lista = True
+list = True
 
-while lista == True :
-    item_usuario = input('\nWhat do you want to add to the list? -Press [Q] to finish- : ')
+while list == True :
+    user_item = input('\nWhat do you want to add to the list? -Press [Q] to finish- : ')
 
-    if item_usuario == 'Q' :
-        lista = False
+    if user_item == 'Q' :
+        list = False
     
     else :
-        if item_usuario in lista_super:
+        if user_item in shopping_list:
                 os.system('clear')
-                print('\n{} is already on the list!'.format(item_usuario))
-                continuar = input('\nPress [Enter] to continue or [Q] to finish ... ')
-                if continuar == 'Q' :
+                print('\n{} is already on the list!'.format(user_item))
+                ok = input('\nPress [Enter] to continue or [Q] to finish ... ')
+                if ok == 'Q' :
                     break
                 os.system('clear')
         
         else :
             os.system('clear')
-            confirmacion = input('\nAre you sure that you want to add {} to the list ? [Y/N]: '.format(item_usuario))
+            confirmation = input('\nAre you sure that you want to add {} to the list ? [Y/N]: '.format(user_item))
         
-            if confirmacion == 'Y' : 
-                lista_super.append(item_usuario)
+            if confirmation == 'Y' : 
+                shopping_list.append(user_item)
                 os.system('clear')
-                print('\nYou added {} to the list.'.format(item_usuario))
-                continuar = input('\nPress [Enter] to continue or [Q] to finish ...')
-                if continuar == 'Q' :
+                print('\nYou added {} to the list.'.format(user_item))
+                ok = input('\nPress [Enter] to continue or [Q] to finish ...')
+                if ok == 'Q' :
                     break
                 os.system('clear')
         
             else:
                 os.system('clear')
-                print('\n{} has not been added to the list.'.format(item_usuario))
-                continuar = input('\nPress [Enter] to continue or [Q] to finish ... ')
-                if continuar == 'Q' :
+                print('\n{} has not been added to the list.'.format(user_item))
+                ok = input('\nPress [Enter] to continue or [Q] to finish ... ')
+                if ok == 'Q' :
                     break
                 os.system('clear')
     
 os.system('clear')
 print('\nYour list is done!: \n')
 
-for item in lista_super:
+for item in shopping_list:
     print('-', item)
 
 print('\nThanks to use Shopping List v1.0 by Nico.')
